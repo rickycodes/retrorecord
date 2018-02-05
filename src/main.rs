a5038e22cfec1a3f827cbba89cf5c28b37780ea6
Ricky Miller <ricky.miller@gmail.com>
2018-02-05 02:23:18 -0500

    post screenshot to twitter

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,20 @@ mod tweet;
 
 fn main() {
     let screenshots = "/home/pi/.config/retroarch/screenshots/";
-    let recordings = "/home/pi/recordings/";
-    if let Ok(_) = tweet::tweet("test!") {
+    // let recordings = "/home/pi/recordings/";
+
+    /* if let Ok(_) = tweet::tweet("test!") {
         println!("posted tweet!");
-    }
+    } */
+
     loop {
-        println!("{:?}", watch::watch(screenshots));
-        println!("{:?}", watch::watch(recordings))
+        // println!("{:?}", watch::watch(screenshots));
+        // println!("{:?}", watch::watch(recordings))
+
+        let path = watch::watch(screenshots);
+        // let path_str = path.to_str();
+        if let Ok(_) = tweet::tweet("test!", path.into_os_string().into_string().unwrap()) {
+            println!("posted tweet!");
+        }
     }
 }
--- src/tweet/mod.rs
+++ src/tweet/mod.rs
@@ -1,10 +1,15 @@
 mod config;
 
 use std::error::Error;
-use egg_mode::{self, KeyPair, Token};
+use std::fs::File;
+use std::io::prelude::*;
+use egg_mode::tweet::DraftTweet;
+use egg_mode::{KeyPair, Token};
+use egg_mode::media::{UploadBuilder, media_types};
 use tokio_core::reactor::{Core};
 
-pub fn tweet(message: &str) -> Result<(), Box<Error>> {
+
+pub fn tweet(message: &str, media_path: String) -> Result<(), Box<Error>> {
     let mut core = Core::new().unwrap();
     let handle = core.handle();
 
@@ -23,8 +28,19 @@ pub fn tweet(message: &str) -> Result<(), Box<Error>> {
         access: acc_token,
     };
 
-    let draft = egg_mode::tweet::DraftTweet::new(message);
-    core.run(draft.send(&token, &handle))?;
+    let mut buffer = Vec::new();
+
+    {
+        let mut file = File::open(media_path.clone()).expect("cannot open picture file..");
+        let _ = file.read_to_end(&mut buffer).expect("cannot read picture file..");
+    }
+
+    let upload_builder = UploadBuilder::new(buffer, media_types::image_png());
+    let media_handler = core.run(upload_builder.call(&token,&handle)).expect("handling media failed..");
+
+    let tweet_draft = DraftTweet::new(message).media_ids(&[media_handler.id]);
+
+    core.run(tweet_draft.send(&token, &handle)).expect("tweet failed..");
 
     Ok(())
 }
