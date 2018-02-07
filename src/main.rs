de716eff5663ebef11d323636cf1791d1f70a0d1
Ricky Miller <ricky.miller@gmail.com>
2018-02-06 20:39:55 -0500

    Threads!

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,8 @@ extern crate tokio_core;
 extern crate notify;
 
 use std::process::{Command, Stdio};
-use std::path::PathBuf;
 use notify::{op, RawEvent};
+use std::thread;
 
 mod watch;
 mod tweet;
@@ -17,51 +17,65 @@ fn main() {
     let screenshots_watcher = watch::watch(&screenshots);
     let recordings_watcher = watch::watch(&recordings);
 
-    loop {
-        match screenshots_watcher.change_events.recv() {
-            Ok(RawEvent{path: Some(path), op: Ok(op), ..}) => {
-                // println!("op is: {:?}", op);
-                if op == op::CLOSE_WRITE {
-                    println!("file written: {:?}", path);
-                    let screenshots_path = path
-                        .into_os_string()
-                        .into_string()
-                        .unwrap();
+    let screenshots_handler = thread::spawn(move|| {
+        loop {
+            match screenshots_watcher.change_events.recv() {
+                Ok(RawEvent{path: Some(path), op: Ok(op), ..}) => {
+                    // println!("op is: {:?}", op);
+                    if op == op::CLOSE_WRITE {
+                        println!("file written: {:?}", path);
+                        let screenshots_path = path
+                            .into_os_string()
+                            .into_string()
+                            .unwrap();
 
-                    if let Ok(_) = tweet::tweet(message::get_message(), screenshots_path) {
-                        println!("posted tweet!");
+                        if let Ok(_) = tweet::tweet(message::get_message(), screenshots_path) {
+                            println!("posted tweet!");
+                        }
                     }
-                }
-            },
-            Ok(event) => println!("broken event: {:?}", event),
-            Err(e) => println!("watch error: {:?}", e),
+                },
+                Ok(event) => println!("broken event: {:?}", event),
+                Err(e) => println!("watch error: {:?}", e),
+            }
         }
+    });
 
-        /* match recordings_watcher.change_events.recv() {
-            Ok(RawEvent{path: Some(path), op: Ok(op), ..}) => {
-                println!("op is: {:?}, path is: {:?}", op, path);
-                if op == op::CLOSE_WRITE {
-                    println!("file written: {:?}", path);
-                    let recordings_path = path_to_string(path);
+    let recordings_handler = thread::spawn(move|| {
+        loop {
+            match recordings_watcher.change_events.recv() {
+                Ok(RawEvent{path: Some(path), op: Ok(op), ..}) => {
+                    println!("op is: {:?}, path is: {:?}", op, path);
+                    if op == op::CLOSE_WRITE {
+                        println!("file written: {:?}", path);
+                        let recordings_path = path
+                            .into_os_string()
+                            .into_string()
+                            .unwrap();
 
-                    let gifs = "/home/pi/gifs/";
-                    let child = Command::new("mkvToGif.sh")
-                        .args(&[recordings_path, gifs.to_string()])
-                        .stdout(Stdio::piped())
-                        .spawn()
-                        .expect("failed to execute child");
+                        let gifs = "/home/pi/gifs/";
+                        let sh = "/home/pi/projects/retrorecord/mkvToGif.sh";
 
-                    let output = child
-                        .wait_with_output()
-                        .expect("failed to wait on child");
+                        let child = Command::new("bash")
+                            .args(&[sh.to_string(), recordings_path, gifs.to_string()])
+                            .stdout(Stdio::piped())
+                            .spawn()
+                            .expect("failed to execute child");
 
-                    if output.status.success() {
-                        println!("gif complete!");
+                        let output = child
+                            .wait_with_output()
+                            .expect("failed to wait on child");
+
+                        if output.status.success() {
+                            println!("gif complete!");
+                        }
                     }
-                }
-            },
-            Ok(event) => println!("broken event: {:?}", event),
-            Err(e) => println!("watch error: {:?}", e),
-        } */
-    }
+                },
+                Ok(event) => println!("broken event: {:?}", event),
+                Err(e) => println!("watch error: {:?}", e),
+            }
+        }
+    });
+
+    screenshots_handler.join().expect("The screenshots thread has panicked");
+    recordings_handler.join().expect("The recordings thread has panicked");
 }
