d2379e9d9c372cb9254a2181bdc917e3c073bb12
Ricky Miller <ricky.miller@gmail.com>
2018-02-10 08:39:52 -0500

    refactor

--- src/main.rs
+++ src/main.rs
@@ -2,93 +2,21 @@ extern crate egg_mode;
 extern crate tokio_core;
 extern crate notify;
 
-use std::process::{Command, Stdio};
-use notify::{op, RawEvent};
-use std::{thread, env};
-use std::path::{Path, PathBuf};
-use egg_mode::media::media_types::{image_gif, image_png};
-
 mod watch;
 mod tweet;
 mod message;
 mod bots;
+mod utils;
+mod screenshots;
+mod recordings;
 
-fn path_to_string(path: PathBuf) -> String {
-    path.into_os_string()
-        .into_string()
-        .unwrap()
-}
+use screenshots::screenshots;
+use recordings::recordings;
 
 fn main() {
-    let screenshots = "/home/pi/.config/retroarch/screenshots/";
-    let recordings = "/home/pi/recordings/";
-
-    let screenshots_watcher = watch::watch(&screenshots);
-    let recordings_watcher = watch::watch(&recordings);
-
-    let screenshots_handler = thread::spawn(move|| {
-        loop {
-            match screenshots_watcher.change_events.recv() {
-                Ok(RawEvent{path: Some(path), op: Ok(op), ..}) => {
-                    // println!("op is: {:?}", op);
-                    if op == op::CLOSE_WRITE {
-                        println!("file written: {:?}", path);
-                        let screenshots_path = path_to_string(path);
-
-                        if let Ok(_) = tweet::tweet(message::get_message(bots::get_bots()), screenshots_path, image_png()) {
-                            println!("posted tweet!");
-                        }
-                    }
-                },
-                Ok(event) => println!("broken event: {:?}", event),
-                Err(e) => println!("watch error: {:?}", e),
-            }
-        }
-    });
-
-    let recordings_handler = thread::spawn(move|| {
-        loop {
-            match recordings_watcher.change_events.recv() {
-                Ok(RawEvent{path: Some(path), op: Ok(op), ..}) => {
-                    println!("op is: {:?}, path is: {:?}", op, path);
-                    if op == op::CLOSE_WRITE {
-                        println!("file written: {:?}", path);
-                        let recordings_path = path_to_string(path);
-
-                        let gifs = "/home/pi/gifs/";
-                        let current_dir = env::current_dir().unwrap();
-
-                        println!("current_dir is: {:?}", current_dir);
-                        let sh = Path::new(&current_dir).join("mkvToGif.sh");
-                        println!("sh is: {:?}", sh);
-
-                        let child = Command::new("bash")
-                            .args(&[path_to_string(sh), recordings_path, gifs.to_string()])
-                            .stdout(Stdio::piped())
-                            .spawn()
-                            .expect("failed to execute child");
-
-                        let output = child
-                            .wait_with_output()
-                            .expect("failed to wait on child");
-
-                        if output.status.success() {
-                            println!("gif complete!");
-                            let output = Path::new(&gifs).join("output.gif");
-                            let gif = path_to_string(output);
-                            println!("gif is {:?}", gif);
-                            if let Ok(_) = tweet::tweet(message::get_message("@DATAM0SHER".to_string()), gif, image_gif()) {
-                                println!("posted tweet!");
-                            }
-                        }
-                    }
-                },
-                Ok(event) => println!("broken event: {:?}", event),
-                Err(e) => println!("watch error: {:?}", e),
-            }
-        }
-    });
-
-    screenshots_handler.join().expect("The screenshots thread has panicked");
-    recordings_handler.join().expect("The recordings thread has panicked");
+    println!("application started...");
+    let screenshots_thread = screenshots();
+    let recordings_thread = recordings();
+    screenshots_thread.join().expect("The screenshots thread has panicked");
+    recordings_thread.join().expect("The recordings thread has panicked");
 }
--- src/tweet/mod.rs
+++ src/tweet/mod.rs
@@ -29,7 +29,6 @@ pub fn get_token() -> Token {
 }
 
 pub fn tweet(message: String, media: String, media_type: Mime) -> Result<(), Box<Error>> {
-    println!("trying to do a tweet");
     let mut core = Core::new().unwrap();
     let handle = core.handle();
 
@@ -37,8 +36,6 @@ pub fn tweet(message: String, media: String, media_type: Mime) -> Result<(), Box
 
     if let Err(err) = core.run(verify_tokens(&token, &handle)) {
         println!("there is something wrong with the tokens: {:?}", err);
-    } else {
-        println!("tokens seem good!");
     }
 
     let mut buffer = Vec::new();
@@ -48,16 +45,10 @@ pub fn tweet(message: String, media: String, media_type: Mime) -> Result<(), Box
         let _ = file.read_to_end(&mut buffer).expect("cannot read picture file..");
     }
 
-    println!("buffer created!");
-
     let upload_builder = UploadBuilder::new(buffer, media_type);
     let media_handler = core.run(upload_builder.call(&token, &handle)).expect("handling media failed..");
 
-    println!("trying to do a draft!");
-
     let tweet = DraftTweet::new(message).media_ids(&[media_handler.id]);
-
-    println!("draft created!");
 
     core.run(tweet.send(&token, &handle)).expect("tweet failed..");
 
