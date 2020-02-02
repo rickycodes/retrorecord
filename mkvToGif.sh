a1f17c6a8f35bff48c06ea4e91c5745d4da6bf98
rickycodes <ricky.miller@gmail.com>
2020-02-02 17:38:36 -0500

    Add shellcheck

--- mkvToGif.sh
+++ mkvToGif.sh
@@ -5,16 +5,16 @@ file=$1
 path=$2
 
 # convert mkv to frames
-ffmpeg -sseof -10 -t 10 -i ${file} -vf scale=${scale}:-1:flags=lanczos,fps=${fps} ${path}ffout%03d.png
+ffmpeg -sseof -10 -t 10 -i "${file}" -vf scale=${scale}:-1:flags=lanczos,fps=${fps} "${path}ffout%03d.png"
 
 # convert frames to gif
 # convert -loop 0 ${path}ffout*.png ${path}output.gif
-gifski --fps ${fps} -o ${path}output.gif ${path}ffout*.png
+gifski --fps ${fps} -o "${path}output.gif" "${path}ffout*.png"
 
 echo "gif created at ${path}output.gif!"
 
 # cleanup
-rm ${path}*.png
-rm ${file}
+rm "${path}*.png"
+rm "${file}"
 
 echo "all done!"
