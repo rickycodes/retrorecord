02c573cb79cefaf9a36b11fabd45874190c7f040
Ricky Miller <ricky.miller@gmail.com>
2021-12-29 09:43:12 -0500

    building

--- build.sh
+++ build.sh
@@ -2,10 +2,10 @@
 
 export CARGO_TARGET_ARMV7_UNKNOWN_LINUX_GNUEABIHF_LINKER=arm-linux-gnueabihf-gcc \
   CC_ARMV7_UNKNOWN_LINUX_GNUEABIHF=arm-linux-gnueabihf-gcc \
-  ARMV7_UNKNOWN_LINUX_GNUEABIHF_OPENSSL_LIB_DIR=/tmp/openssl-1.1.0l \
-  ARMV7_UNKNOWN_LINUX_GNUEABIHF_OPENSSL_INCLUDE_DIR=/tmp/openssl-1.1.0l/include \
+  ARMV7_UNKNOWN_LINUX_GNUEABIHF_OPENSSL_LIB_DIR=~/openssl/openssl-1.1.1k \
+  ARMV7_UNKNOWN_LINUX_GNUEABIHF_OPENSSL_INCLUDE_DIR=~/openssl/openssl-1.1.1k/include \
   TARGET_CC=arm-linux-gnueabihf-gcc \
   TARGET_CXX=arm-linux-gnueabihf-g++ \
-  OPENSSL_DIR=/tmp/openssl-1.1.0l
+  OPENSSL_DIR=~/openssl/openssl-1.1.1k
 
 cargo build --target=armv7-unknown-linux-gnueabihf --release
